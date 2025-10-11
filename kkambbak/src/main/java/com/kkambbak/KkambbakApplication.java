0d467a4b440477d2c6a18ba1f189dcfbf8abe4de
kjunh972 <kjunh972@gmail.com>
2025-10-11 15:09:13 +0900

    Feat: [init] 개발 환경 구축

---
+++ kkambbak/src/main/java/com/kkambbak/KkambbakApplication.java
@@ -0,0 +1,13 @@
+package com.kkambbak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkambbakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkambbakApplication.class, args);
+	}
+
+}
