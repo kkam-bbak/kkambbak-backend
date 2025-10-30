a2bd348b221f8ccf1f872cd47a228ef0fd2ffde9
kjunh972 <kjunh972@gmail.com>
2025-10-31 00:27:37 +0900

    Feat: [monitoring] 성능 모니터링 로그 분석 모니터링 구축

--- kkambbak/src/main/java/com/kkambbak/global/config/LogbookConfig.java
+++ kkambbak/src/main/java/com/kkambbak/global/config/LogbookConfig.java
@@ -60,12 +60,34 @@ public class LogbookConfig {
 
         @Override
         public void write(Precorrelation precorrelation, String request) {
-            logger.trace(request);
+            logger.info(request);
         }
 
         @Override
         public void write(Correlation correlation, String response) {
-            logger.trace(response);
+            String level = extractLevel(response);
+            switch (level) {
+                case "ERROR":
+                    logger.error(response);
+                    break;
+                case "WARN":
+                    logger.warn(response);
+                    break;
+                default:
+                    logger.info(response);
+            }
+        }
+
+        private String extractLevel(String json) {
+            int levelStart = json.indexOf("\"level\":\"");
+            if (levelStart != -1) {
+                levelStart += 9;
+                int levelEnd = json.indexOf("\"", levelStart);
+                if (levelEnd != -1) {
+                    return json.substring(levelStart, levelEnd);
+                }
+            }
+            return "INFO";
         }
     }
 
