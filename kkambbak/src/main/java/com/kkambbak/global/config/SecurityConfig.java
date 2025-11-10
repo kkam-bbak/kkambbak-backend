601fe85b444aae1570ac911127205dda08a1ac24
kjunh972 <kjunh972@gmail.com>
2025-11-11 01:26:19 +0900

    Feat: [Cors] CORS 오류 해결

--- kkambbak/src/main/java/com/kkambbak/global/config/SecurityConfig.java
+++ kkambbak/src/main/java/com/kkambbak/global/config/SecurityConfig.java
@@ -48,6 +48,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configure(http))
                 .csrf(csrf -> csrf.disable())
                 .formLogin(formLogin -> formLogin.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
